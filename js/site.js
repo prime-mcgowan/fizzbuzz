3b52bf6fb92fd3f44b0d48ffcf7cc757d69e87ce
Laura McGowan <prime.lmcg@gmail.com>
2023-08-23 13:32:05 -0500

    fizzBuzz and displayData functions are written and working

--- js/site.js
+++ js/site.js
@@ -13,6 +13,7 @@ function getValues() {
     //call fizzBuzz
     let fbArray = fizzBuzz(fizzValue, buzzValue);
     //call displayData
+    displayData(fbArray);
   } else {
     alert("You must enter numbers");
   }
@@ -41,10 +42,40 @@ function fizzBuzz(fizzValue, buzzValue) {
     }
   }
   //return the array
-  returnArray;
+  return returnArray;
 }
 
 function displayData(fbArray) {
+  //get table body element from the page
+  let tableBody = document.getElementById("results");
+
+  //get the template rows
+  let templateRow = document.getElementById("fbTemplate");
+
+  //clear table first
+  tableBody.innerHTML = "";
+
   //loop over the array and create a tablerow for each item
-  //add all the rows to the table
+  for (let index = 0; index < fbArray.length; index += 5) {
+    let tableRow = document.importNode(templateRow.content, true);
+
+    let rowCols = tableRow.querySelectorAll("td");
+
+    rowCols[0].classList.add(fbArray[index]);
+    rowCols[0].textContent = fbArray[index];
+
+    rowCols[1].classList.add(fbArray[index + 1]);
+    rowCols[1].textContent = fbArray[index + 1];
+
+    rowCols[2].classList.add(fbArray[index + 2]);
+    rowCols[2].textContent = fbArray[index + 2];
+
+    rowCols[3].classList.add(fbArray[index + 3]);
+    rowCols[3].textContent = fbArray[index + 3];
+
+    rowCols[4].classList.add(fbArray[index + 4]);
+    rowCols[4].textContent = fbArray[index + 4];
+
+    tableBody.appendChild(tableRow);
+  }
 }
